0ff1a3c6b3dd4033eeb8b0a9ab7c3b77856c5f70
Najmiddin Nazirov <nnazirov@transoxiania.com>
2023-08-29 07:46:39 +0500

    modal & player

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { AiOutlineSearch, AiOutlineUser } from 'react-icons/ai';
+import { useContext, useEffect, useState } from 'react';
+import { AiOutlineSearch, AiOutlineUser, AiOutlineLogout } from 'react-icons/ai';
 import { BiBellMinus } from 'react-icons/bi';
+import { AuthContext } from 'src/context/auth.context';
 
 const Header = () => {
 	const [scrolled, setScrolled] = useState(false);
+	const { logout } = useContext(AuthContext);
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -22,7 +24,7 @@ const Header = () => {
 	}, []);
 
 	return (
-		<header className={`${scrolled && 'bg-[#E10856] shadow-lg'}`}>
+		<header className={`${scrolled && 'bg-[#141414] shadow-lg'}`}>
 			<div className='flex items-center space-x-2 md:space-x-10'>
 				<Image src={'/logo.svg'} alt={'logo'} width={56} height={56} className={'cursor-pointer object-contain'} />
 
@@ -42,6 +44,7 @@ const Header = () => {
 				<Link href={'/account'}>
 					<AiOutlineUser className='h-6 w-6 cursor-pointer' />
 				</Link>
+				<AiOutlineLogout className='h-6 w-6 cursor-pointer' onClick={logout} />
 			</div>
 		</header>
 	);
---
+++ src/firebase/index.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgsqSPwKMi4AhDkEU8CQuQh321ECP5OFg",
+    authDomain: "mern-sammi-1.firebaseapp.com",
+    projectId: "mern-sammi-1",
+    storageBucket: "mern-sammi-1.appspot.com",
+    messagingSenderId: "786811767869",
+    appId: "1:786811767869:web:9e98793bdde65d6c76ae54",
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { db, auth };
